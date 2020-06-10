5ef573293eaa93ca682e187694537e81da656c03
Serafeim Papastefanos <spapas@gmail.com>
2020-06-10 19:13:54 +0300

    ok

--- src/ringu/handlers.clj
+++ src/ringu/handlers.clj
@@ -1,8 +1,8 @@
 (ns ringu.handlers
-  (:require [ring.util.response :refer [response]]))            
-            
-            
+  (:require [ring.util.response :refer [response]])
+  (:require [ringu.db.authorities :as au])
+  (:require [ringu.db.core :as db]))
 
 
 (defn home [_]
-  (response "HELLO HOME"))
+  (response (au/all-authorities (db/db-connection))))
