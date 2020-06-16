bce864124ba5bdc2f055ab6f951589ecbdae9839
Serafeim Papastefanos <spapas@gmail.com>
2020-06-16 09:04:20 +0300

    Remove unneeded code

--- src/ringu/web/handlers.clj
+++ src/ringu/web/handlers.clj
@@ -1,18 +1,6 @@
 (ns ringu.web.handlers
-  (:require [ring.util.response :refer [response content-type]]
-            [ringu.web.views.home :as home]
-            [ringu.web.views.suppliers :as suppliers]))
+  (:require [ring.util.response :refer [response content-type]]))
 
-
-(defn home [req]
-  (content-type
-   (response (home/index-page req))
-   "text/html; charset=utf-8"))
-
-(defn suppliers [req]
-  (content-type
-   (response (suppliers/index-page req))
-   "text/html; charset=utf-8"))
 
 (defn html-resp [f]
   (fn [req]
