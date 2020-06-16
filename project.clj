216a1dd6d73cee22da42aa69a07cf66412709bc0
Serafeim Papastefanos <spapas@gmail.com>
2020-06-16 12:58:30 +0300

    Add managed dependencies to project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :managed-dependencies [[ring/ring-core "1.8.1"]
+                         [org.clojure/java.jdbc "0.7.10"]
+                         [borkdude/edamame "0.0.7"]
+                         [org.clojure/spec.alpha "0.2.187"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.8.1"]
+                 ;[ring/ring-core "1.8.1"]
                  [ring/ring-devel "1.8.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring/ring-jetty-adapter "1.8.1"]
