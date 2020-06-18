23d6566eb26616ffa017b2b620088707ed9ed7a1
Serafeim Papastefanos <spapas@gmail.com>
2020-06-18 11:45:58 +0300

    Some changes to see how tests work

--- src/ringu/dev.clj
+++ src/ringu/dev.clj
@@ -2,6 +2,7 @@
   (:require [ringu.db.suppliers :as su])
   (:require [ringu.conf.util :as conf])
   (:require [ringu.db.core :as db])
+  (:require [clojure.test :as test])
   (:require [ringu.web.views.helpers :as wh]))
 
 (def db db/db-connection)
@@ -11,7 +12,9 @@
 (def iauk su/insert-get-id)
 (def get-path wh/get-path)
 
+(defn runtest []
+  (test/run-tests 'ringu.core-test))
+
 (comment
   (lsu (db) {:name "ΚΛ ΠΕΙΡΑΙΑ 2"})
-  (su/all-suppliers (db) {})
-  )
+  (su/all-suppliers (db) {}))
